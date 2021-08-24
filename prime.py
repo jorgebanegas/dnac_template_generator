fc1329d5d606ea1f366b1a5e419a5dbd84cd05c8
Jorge Banegas <jjb87@miami.edu>
2021-08-24 13:26:39 -0400

    Add files via upload

--- prime.py
+++ prime.py
@@ -7,10 +7,20 @@ from config import PI, USER, PASSWORD
 import pprint
 from netmiko import ConnectHandler
 from dnacentersdk import api
+import sys
 
 base_url = 'https://' + PI + '/webacs/api/v4/data'
 
-url = base_url + '/BulkSanitizedConfigArchives.json'
+option = input("Query by device ip address (I) or by device hostname (H) : ")
+if str(option).lower() == 'i':
+    value = input("Enter device ip address : ")
+    url = base_url + '/BulkSanitizedConfigArchives.json?deviceIpAddress=' + str(value)
+elif str(option).lower() == 'h':
+    value = input("Enter device hostname : ")
+    url = base_url + '/BulkSanitizedConfigArchives.json?deviceName=' + str(value)
+else:
+    print("wrong input")
+    sys.exit()
 
 payload = {}
 headers = {
@@ -20,19 +30,11 @@ response = requests.request('GET', url,auth=HTTPBasicAuth(USER, PASSWORD),verify
 
 response = json.loads(response.text)
 
-pprint.pprint(response)
-
-file_id = input("Select file ID to download configuration : ")
-
-base_url = 'https://' + PI + '/webacs/api/v4/op'
-url = base_url + '/configArchiveService/extractSanitizedFile.json?fileId=' + file_id
-
-payload = {
-}
-headers = {
-}
-response = requests.request('GET', url,auth=HTTPBasicAuth(USER, PASSWORD),verify=False, headers=headers, data = payload)
-
-response = json.loads(response.text)
-
-pprint.pprint(response)
+file = response['queryResponse']["entityId"][0]
+file_request = requests.request('GET', file["@url"]+".json",auth=HTTPBasicAuth(USER, PASSWORD),verify=False, headers=headers, data = payload)
+file_request = json.loads(file_request.text)
+files = file_request['queryResponse']['entity'][0]['bulkSanitizedConfigArchivesDTO']['files']['file']
+for x in files :
+    if x['fileState'] == 'STARTUPCONFIG':
+        with open('config.txt', 'w') as f:
+            f.write(x['data'])
